60f9cd52e0d4036064f1a36ac17bdd112d7e3789
Bruno Rodrigues Lima <brunolima.18@outlook.com>
2021-02-12 16:05:51 -0300

    Add project files.

---
+++ Controllers/InfectadoController.cs
@@ -0,0 +1,38 @@
+﻿using DIODotNetMongo.Data;
+using DIODotNetMongo.Data.Collections;
+using DIODotNetMongo.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System.Linq;
+
+namespace DIODotNetMongo.Controllers
+{
+    [Route("{controller}")]
+    [ApiController]
+    public class InfectadoController : ControllerBase
+    {
+        private readonly MongoDBContext _mongo;
+        private readonly IMongoCollection<Infectado> _collection;
+
+        public InfectadoController(MongoDBContext mongo)
+        {
+            _mongo = mongo;
+            _collection = _mongo.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+            _collection.InsertOne(infectado);
+            return Created("", "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _collection.Find(Builders<Infectado>.Filter.Empty).ToList();
+            return Ok(infectados);
+        }
+    }
+}
---
+++ Data/MongoDBContext.cs
@@ -0,0 +1,45 @@
+﻿using DIODotNetMongo.Data.Collections;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using System;
+
+namespace DIODotNetMongo.Data
+{
+    public class MongoDBContext
+    {
+        public IMongoDatabase DB { get; set; }
+
+        public MongoDBContext(IConfiguration configuration)
+        {
+            try
+            {
+                var settings = MongoClientSettings.FromConnectionString(configuration["ConnectionString"]);
+                var client = new MongoClient(settings);
+                DB = client.GetDatabase(configuration["DatabaseName"]);
+                MapClasses();
+            }
+            catch (Exception e)
+            {
+                throw new MongoException("It was not possible to connect to MongoDB", e);
+            }
+        }
+
+        private static void MapClasses()
+        {
+            var conventionPack = new ConventionPack { new CamelCaseElementNameConvention() };
+
+            ConventionRegistry.Register("camelCase", conventionPack, filter => true);
+
+            if (!BsonClassMap.IsClassMapRegistered(typeof(Infectado)))
+            {
+                BsonClassMap.RegisterClassMap<Infectado>(config =>
+                {
+                    config.AutoMap();
+                    config.SetIgnoreExtraElements(true);
+                });
+            }
+        }
+    }
+}
